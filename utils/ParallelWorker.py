423a5fdde8fe18e3877dd72b56e1466b31a5bb22
jarodf <jarodf>
2025-01-10 10:18:26 +0800

    num worker 8

--- utils/ParallelWorker.py
+++ utils/ParallelWorker.py
@@ -174,7 +174,7 @@ class ParallelWorker:
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--num_worker", type=int, default=1)
+    parser.add_argument("--num_worker", type=int, default=8)
     parser.add_argument("--agent_num", type=int, default=5)
     parser.add_argument("--agent_dim", type=int, default=3)
     parser.add_argument("--hidden_dim", type=int, default=128)
