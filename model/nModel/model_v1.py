18c97ff76cb508dab8ee1feeb62076ff9180ac52
jarodf <jarodf>
2025-02-20 18:06:32 +0800

    simple ENV model fit

---
+++ model/nModel/model_v1.py
@@ -0,0 +1,231 @@
+import argparse
+
+import numpy as np
+import torch
+import torch.nn as nn
+
+import model.Base.Net as Net
+from model.Base.Net import MultiHeadAttentionLayer, SingleHeadAttention, CrossAttentionLayer
+
+
+class CityEmbedding(nn.Module):
+    def __init__(self, input_dim, hidden_dim, embed_dim):
+        super(CityEmbedding, self).__init__()
+        self.embed_dim = embed_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+        self.depot_embed = nn.Linear(self.input_dim, self.embed_dim)
+        self.city_embed = nn.Linear(self.input_dim, self.embed_dim)
+        # self.depot_embed = nn.Sequential(
+        #     nn.Linear(self.input_dim, self.hidden_dim),
+        #     nn.ReLU(),
+        #     nn.Linear(self.hidden_dim, self.embed_dim),
+        # )
+        #
+        # self.city_embed = nn.Sequential(
+        #     nn.Linear(self.input_dim, self.embed_dim),
+        #     nn.ReLU(),
+        #     nn.Linear(self.embed_dim, self.embed_dim),
+        # )
+
+    def forward(self, city):
+        """
+        :param city: [B,N,2]
+        :return:
+        """
+        depot_embed = self.depot_embed(city[:, 0:1])
+        city_embed = self.city_embed(city[:, 1:])
+        cities_embed = torch.cat([depot_embed, city_embed], dim=1)
+        return cities_embed
+
+
+class CityEncoder(nn.Module):
+    def __init__(self, input_dim=2, hidden_dim=256, embed_dim=128, num_heads=4, num_layers=2):
+        super(CityEncoder, self).__init__()
+        self.city_embed = CityEmbedding(input_dim, hidden_dim, embed_dim)
+        self.city_self_att = nn.Sequential(
+            *[
+                MultiHeadAttentionLayer(num_heads, embed_dim, hidden_dim)
+                for _ in range(num_layers)
+            ]
+        )
+
+    def forward(self, city):
+        """
+        :param city: [B,N,2]
+        :return:
+        """
+        city_embed = self.city_embed(city)
+        city_self_att = self.city_self_att(city_embed)
+        return city_self_att
+
+class AgentEmbedding(nn.Module):
+    def __init__(self, input_dim, hidden_dim, embed_dim):
+        super(AgentEmbedding, self).__init__()
+        self.embed_dim = embed_dim
+        self.input_dim = input_dim
+        self.hidden_dim = hidden_dim
+
+        self.depot_embed = nn.Linear(2, self.embed_dim)
+        self.cur_city_embed = nn.Linear(2, self.embed_dim)
+        self.distance_embed = nn.Linear(2, self.embed_dim)
+        self.graph_embed = nn.Linear(self.embed_dim, self.embed_dim)
+
+        self.agent_embed = nn.Linear(4 * self.embed_dim, self.embed_dim)
+
+    def forward(self, graph_embed, agent_state):
+        """
+        :param graph_embed
+        :param agent_state: [B,M,2]
+        :return:
+        """
+        global_graph_embed = self.graph_embed(graph_embed)
+        depot_embed = self.depot_embed(agent_state[:,:,0:2])
+        cur_city_embed = self.cur_city_embed(agent_state[:,:,2:4])
+        distance_embed = self.distance_embed(agent_state[:,:,4:6])
+        context = torch.cat([depot_embed, cur_city_embed, distance_embed, global_graph_embed], dim=-1)
+        agent_embed = self.agent_embed(context)
+        return agent_embed
+
+
+class AgentEncoder(nn.Module):
+    def __init__(self, input_dim=2, hidden_dim=256, embed_dim=128, num_heads=4, num_layers=2):
+        super(AgentEncoder, self).__init__()
+        self.agent_embed = AgentEmbedding(input_dim, hidden_dim, embed_dim)
+        self.agent_self_att = nn.Sequential(
+            *[
+                MultiHeadAttentionLayer(num_heads, embed_dim, hidden_dim)
+                for _ in range(num_layers)
+            ]
+        )
+
+    def forward(self, graph, agent):
+        """
+        :param agent: [B,N,2]
+        :return:
+        """
+        agent_embed = self.agent_embed(graph, agent)
+        agent_self_att = self.agent_self_att(agent_embed)
+        return agent_self_att
+
+class ActionDecoder(nn.Module):
+    def __init__(self, hidden_dim=256, embed_dim=128, num_heads=4, num_layers=2):
+        super(ActionDecoder, self).__init__()
+        self.agent_city_att = CrossAttentionLayer(embed_dim, num_heads, use_FFN=True, hidden_size=hidden_dim)
+        self.linear_forward = nn.Linear(embed_dim, embed_dim)
+        self.action = SingleHeadAttention(embed_dim)
+        self.num_heads = num_heads
+
+    def forward(self, agent_embed, city_embed, masks):
+        expand_city_embed = city_embed.expand(agent_embed.size(0), -1, -1)
+        expand_masks = masks.unsqueeze(1).expand(agent_embed.size(0), self.num_heads, -1, -1)
+        expand_masks = expand_masks.reshape(agent_embed.size(0) * self.num_heads, expand_masks.size(2), expand_masks.size(3))
+        aca = self.agent_city_att(agent_embed, expand_city_embed, expand_city_embed, expand_masks)
+        cross_out = self.linear_forward(aca)
+        action_logits = self.action(cross_out, expand_city_embed, masks)
+        return action_logits
+
+
+class ActionReselector(nn.Module):
+    def __init__(self, embed_dim=128, num_heads=4, num_layers=2):
+        super(ActionReselector, self).__init__()
+        self.act_to_agent = SingleHeadAttention(embed_dim)
+
+    def forward(self, agent_embed, city_embed):
+        city_to_agent = self.act_to_agent(city_embed, agent_embed)
+        agent = torch.argmax(city_to_agent, dim=-1)
+        return agent
+
+
+class Model(nn.Module):
+    def __init__(self, agent_dim=6, hidden_dim=256, embed_dim=128, num_heads=4, num_layers=2):
+        super(Model, self).__init__()
+        self.city_encoder = CityEncoder(2, hidden_dim, embed_dim, num_heads, num_layers)
+        self.agent_encoder = AgentEncoder(agent_dim, hidden_dim, embed_dim, num_heads, num_layers)
+        self.agent_decoder = ActionDecoder(hidden_dim, embed_dim, num_heads, num_layers)
+        self.action_reselector = ActionReselector(embed_dim, num_heads, num_layers)
+        self.city_embed = None
+
+    def init_city(self, city):
+        """
+        :param city: [B,N,2]
+        :return: None
+        """
+        self.city_embed = self.city_encoder(city)
+
+    def forward(self, agent, mask):
+        graph = torch.mean(self.city_embed, dim=1)
+        expand_graph = graph.unsqueeze(0).expand(agent.size(0), agent.size(1), -1)
+        agent_embed = self.agent_encoder(expand_graph, agent)
+        actions_logits = self.agent_decoder(agent_embed, self.city_embed, mask)
+
+        # expanded_city_embed = self.city_embed.expand(select.size(1), -1, -1)
+        # expanded_select = select.unsqueeze(-1).expand(-1,-1,128)
+        # select_city_embed = torch.gather(expanded_city_embed,1, expanded_select)
+        # reselect = self.action_reselector(agent_embed, select_city_embed)
+        return actions_logits
+
+
+if __name__ == "__main__":
+    from envs.MTSP.MTSP2 import MTSPEnv
+    from utils.TensorTools import _convert_tensor
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_worker", type=int, default=4)
+    parser.add_argument("--agent_num", type=int, default=5)
+    parser.add_argument("--agent_dim", type=int, default=3)
+    parser.add_argument("--hidden_dim", type=int, default=256)
+    parser.add_argument("--embed_dim", type=int, default=256)
+    parser.add_argument("--num_heads", type=int, default=4)
+    parser.add_argument("--num_layers", type=int, default=3)
+    parser.add_argument("--gamma", type=float, default=1)
+    parser.add_argument("--lr", type=float, default=5e-5)
+    parser.add_argument("--grad_max_norm", type=float, default=0.5)
+    parser.add_argument("--cuda_id", type=int, default=0)
+    parser.add_argument("--use_gpu", type=bool, default=True)
+    parser.add_argument("--returns_norm", type=bool, default=True)
+    parser.add_argument("--max_ent", type=bool, default=True)
+    parser.add_argument("--entropy_coef", type=float, default=1e-2)
+    parser.add_argument("--batch_size", type=float, default=512)
+    parser.add_argument("--city_nums", type=int, default=50)
+    parser.add_argument("--allow_back", type=bool, default=False)
+    parser.add_argument("--model_dir", type=str, default="../pth/")
+    parser.add_argument("--agent_id", type=int, default=0)
+    args = parser.parse_args()
+
+    cfg = {
+        "salesmen": args.agent_num,
+        "cities": args.city_nums,
+        "seed": None,
+        "mode": 'rand'
+    }
+    env = MTSPEnv(
+        cfg
+    )
+    states, info = env.reset()
+    anum = info["salesmen"]
+    cnum = info["cities"]
+    graph = info["graph"]
+    global_mask = info["mask"]
+    agents_mask = info["mask"][np.newaxis].repeat(anum, axis=0)
+
+    done = False
+    EndInfo = {}
+    EndInfo.update(info)
+    agent_config = {
+        "city_nums": cnum,
+        "agent_nums": anum,
+    }
+    graph_t = _convert_tensor(graph, device="cuda", target_shape_dim=3)
+    from algorithm.DNN2.AgentBase import AgentBase
+
+    agent = AgentBase(args, Model)
+    agent.reset_graph(graph_t)
+
+    states_nb, actions_nb, returns_nb, masks_nb, done_nb = (
+        agent._run_episode(env, graph, anum, eval_mode=False, exploit_mode="greedy"))
+
+    from utils.GraphPlot import GraphPlot as GP
+
+    gp = GP()
+    # gp.draw_route(graph, EndInfo["trajectories"], title="random", one_first=True)
---
+++ utils/ShareWorker2.py
@@ -0,0 +1,279 @@
+import sys
+import time
+
+import numpy as np
+import sys
+
+sys.path.append("../")
+sys.path.append("./")
+import envs.MTSP.Config
+from envs.MTSP.MTSP import MTSPEnv
+from typing import Dict
+import cloudpickle
+
+import torch.multiprocessing as mp
+from envs.GraphGenerator import GraphGenerator as GG
+from utils.TensorTools import _convert_tensor
+import torch
+from torch.utils.tensorboard import SummaryWriter
+from datetime import datetime
+from algorithm.OR_Tools.mtsp import ortools_solve_mtsp
+import argparse
+from envs.MTSP.MTSP import MTSPEnv
+from algorithm.DNN.Agent_V3 import AgentV3 as Agent
+# from algorithm.DNN.Agent_v1 import AgentV1 as Agent
+import tqdm
+
+torch.set_num_threads(1)
+
+
+def worker_process(share_agent, agent_class, args, env_class, env_config, recv_pipe, queue):
+    env = env_class(env_config)
+    work_agent = share_agent
+    # work_agent = agent_class(args)
+    while True:
+        graph = recv_pipe.recv()
+        # work_agent.model.load_state_dict(share_agent.model.state_dict())
+        features_nb, actions_nb, returns_nb, masks_nb, dones_nb = work_agent.run_batch(env, graph, args.agent_num,
+                                                                              args.batch_size // args.num_worker)
+        queue.put((graph, features_nb, actions_nb, returns_nb, masks_nb, dones_nb))
+
+
+def eval_process(share_agent, agent_class, args, env_class, env_config, recv_model_pipe, send_result_pipe, sample_times):
+    env = env_class(env_config)
+    eval_agent = share_agent
+    # eval_agent = agent_class(args)
+    print(eval_agent.device)
+    while True:
+        graph = recv_model_pipe.recv()
+        # eval_agent.model.load_state_dict(share_agent.model.state_dict())
+        st = time.time_ns()
+        greedy_cost, greedy_trajectory = eval_agent.eval_episode(env, graph, args.agent_num, exploit_mode="greedy")
+        ed = time.time_ns()
+        greedy_time = (ed - st) / 1e9
+        min_sample_cost = np.inf
+        min_sample_trajectory = None
+        st = time.time_ns()
+        for i in range(sample_times):
+            sample_cost, sample_trajectory = eval_agent.eval_episode(env, graph, args.agent_num, exploit_mode="sample")
+            if sample_cost < min_sample_cost:
+                min_sample_cost = sample_cost
+                min_sample_trajectory = sample_trajectory
+        ed = time.time_ns()
+        sample_time = (ed - st) / 1e9
+
+        ortools_trajectory, ortools_cost, used_time = ortools_solve_mtsp(graph, args.agent_num, 10000)
+        env.draw_multi(graph,
+                       [ortools_cost, greedy_cost, min_sample_cost],
+                       [ortools_trajectory, greedy_trajectory, min_sample_trajectory],
+                       [used_time, greedy_time, sample_time],
+                       ["or_tools", "greedy", "sample"]
+                       )
+
+        send_result_pipe.send(
+            (greedy_cost, greedy_trajectory, min_sample_cost, min_sample_trajectory, ortools_cost, ortools_trajectory))
+
+
+def train_process(share_agent, agent_class, agent_args, send_pipes, queue, eval_model_pipe, eval_result_pipe):
+    # agent_args.use_gpu = False
+
+    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    writer = SummaryWriter(f"../log/workflow-{timestamp}")
+    writer.add_text("agent_config", str(agent_args), 0)
+
+    eval_count = 0
+    train_count = 0
+    graphG = GG(1, agent_args.city_nums)
+
+    train_agent = agent_class(agent_args)
+    model_state_dict = share_agent.model.state_dict()
+    train_agent.model.load_state_dict(model_state_dict)
+
+    for _ in tqdm.tqdm(range(100_000_000)):
+        graph = graphG.generate()
+        # if (train_count+1) % agent_args.num_worker == 0:
+        for pipe in send_pipes:
+            pipe.send(graph)
+
+        last_state_nb_list = []
+        state_nb_list = []
+        actions_nb_list = []
+        returns_nb_list = []
+        masks_nb_list = []
+        dones_nb_list = []
+
+        for i in range(agent_args.num_worker):
+            graph, features_nb, actions_nb, returns_nb, masks_nb, dones_nb = queue.get()
+            last_state_nb_list.append(features_nb[0])
+            state_nb_list.append(features_nb[1])
+            actions_nb_list.append(actions_nb)
+            returns_nb_list.append(returns_nb)
+            masks_nb_list.append(masks_nb)
+            dones_nb_list.append(dones_nb)
+        features_nb = [np.concatenate(last_state_nb_list, axis=0), np.concatenate(state_nb_list, axis=0)]
+        actions_nb = np.concatenate(actions_nb_list, axis=0)
+        returns_nb = np.concatenate(returns_nb_list, axis=0)
+        masks_nb = np.concatenate(masks_nb_list, axis=0)
+        dones_nb = np.concatenate(dones_nb_list,axis = 0)
+        train_agent.model.load_state_dict(share_agent.model.state_dict())
+        loss = train_agent.learn(_convert_tensor(graph, dtype=torch.float32, device=train_agent.device, target_shape_dim=3),
+                           _convert_tensor(features_nb, dtype=torch.float32, device=train_agent.device),
+                           _convert_tensor(actions_nb, dtype=torch.float32, device=train_agent.device),
+                           _convert_tensor(returns_nb, dtype=torch.float32, device=train_agent.device),
+                           _convert_tensor(masks_nb, dtype=torch.float32, device=train_agent.device),
+                            dones_nb
+                           )
+        writer.add_scalar("loss", loss, train_count)
+        torch.cuda.empty_cache()  # 清理未使用的缓存
+        share_agent.model.load_state_dict(train_agent.model.state_dict())
+
+        if (train_count + 1) % 100 == 0:
+            eval_count = train_count
+            # graph = graphG.generate()
+            eval_model_pipe.send(graph)
+
+        train_count += 1
+
+        if (train_count +1)% 100 == 0 and eval_result_pipe.poll():
+            greedy_cost, greedy_trajectory, min_sample_cost, min_sample_trajectory, ortools_cost, ortools_trajectory = eval_result_pipe.recv()
+            writer.add_scalar("greedy_cost", greedy_cost, eval_count)
+            writer.add_scalar("min_sample_cost", min_sample_cost, eval_count)
+            writer.add_scalar("ortools_cost", ortools_cost, eval_count)
+            greedy_gap = (greedy_cost - ortools_cost) / ortools_cost * 100
+            sample_gap = (min_sample_cost - ortools_cost) / ortools_cost * 100
+            writer.add_scalar("greedy_gap", greedy_gap, eval_count)
+            writer.add_scalar("sample_gap", sample_gap, eval_count)
+
+            print(f"greddy_cost:{greedy_cost},{greedy_gap}%, sample_cost:{min_sample_cost},{sample_gap}%, ortools_cost:{ortools_cost}")
+
+        if (train_count + 1) % 5000 == 0:
+            train_agent.save_model(train_count + 1 + agent_args.agent_id)
+
+
+class SharelWorker:
+    def __init__(self, agent_class, args, env_class):
+        self.agent_class = agent_class
+        self.agent_args = args
+        self.env_class = env_class
+        self.env_config = {
+
+            "city_nums": (args.city_nums, args.city_nums),
+            "agent_nums": (args.agent_num, args.agent_num),
+            "allow_back": args.allow_back,
+        }
+        self.num_worker = args.num_worker
+
+        self.queue = mp.Queue()
+        self.worker_pipes = [mp.Pipe(duplex=False) for _ in range(self.num_worker)]
+        self.eval_model_pipes = mp.Pipe(duplex=False)
+        self.eval_result_pipes = mp.Pipe(duplex=False)
+        args.use_gpu = False
+        self.share_agent = agent_class(args)
+        self.share_agent.load_model(args.agent_id)
+        self.share_agent.model.share_memory()
+        args.use_gpu = True
+
+    def run(self):
+        worker_processes = [mp.Process(target=worker_process,
+                                       args=(self.share_agent,
+                                             self.agent_class,
+                                             self.agent_args,
+                                             self.env_class,
+                                             self.env_config,
+                                             self.worker_pipes[worker_id][0],
+                                             self.queue))
+                            for worker_id in range(self.num_worker)
+                            ]
+
+        trainer_process = mp.Process(target=train_process,
+                                     args=(self.share_agent,
+                                           self.agent_class,
+                                           self.agent_args,
+                                           [pipe[1] for pipe in self.worker_pipes],
+                                           self.queue,
+                                           self.eval_model_pipes[1],
+                                           self.eval_result_pipes[0]
+                                           )
+                                     )
+
+        evaler_process = mp.Process(target=eval_process,
+                                    args=(self.share_agent,
+                                          self.agent_class,
+                                          self.agent_args,
+                                          self.env_class,
+                                          self.env_config,
+                                          self.eval_model_pipes[0],
+                                          self.eval_result_pipes[1],
+                                          64
+                                          )
+                                    )
+
+        trainer_process.start()
+        # time.sleep(10)
+        for p in worker_processes:
+            p.start()
+        # time.sleep(10)
+        evaler_process.start()
+
+        import signal
+
+        # 自定义的信号处理函数
+        def signal_handler(sig, frame):
+            trainer_process.terminate()
+            trainer_process.join()
+            evaler_process.terminate()
+            evaler_process.join()
+            for task in worker_processes:
+                task.terminate()
+                task.join()
+            print("Received signal to terminate the program.")
+            # 执行必要的清理操作
+
+        # 注册信号处理器
+        signal.signal(signal.SIGTERM, signal_handler)
+        signal.signal(signal.SIGINT, signal_handler)  # 也可以处理Ctrl+C终止
+
+        trainer_process.join()
+        for p in worker_processes:
+            p.join()
+
+        evaler_process.join()
+        for pipe in self.worker_pipes:
+            pipe[0].close()
+            pipe[1].close()
+        self.eval_model_pipes[0].close()
+        self.eval_model_pipes[1].close()
+        self.eval_result_pipes[0].close()
+        self.eval_result_pipes[1].close()
+
+        sys.exit(0)  # 正常退出程序
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--num_worker", type=int, default=4)
+    parser.add_argument("--agent_num", type=int, default=5)
+    parser.add_argument("--agent_dim", type=int, default=3)
+    parser.add_argument("--hidden_dim", type=int, default=256)
+    parser.add_argument("--embed_dim", type=int, default=256)
+    parser.add_argument("--num_heads", type=int, default=4)
+    parser.add_argument("--num_layers", type=int, default=3)
+    parser.add_argument("--gamma", type=float, default=1)
+    parser.add_argument("--lr", type=float, default=5e-5)
+    parser.add_argument("--grad_max_norm", type=float, default=0.5)
+    parser.add_argument("--cuda_id", type=int, default=0)
+    parser.add_argument("--use_gpu", type=bool, default=True)
+    parser.add_argument("--returns_norm", type=bool, default=True)
+    parser.add_argument("--max_ent", type=bool, default=True)
+    parser.add_argument("--entropy_coef", type=float, default=1e-2)
+    parser.add_argument("--batch_size", type=float, default=512)
+    parser.add_argument("--city_nums", type=int, default=50)
+    parser.add_argument("--allow_back", type=bool, default=False)
+    parser.add_argument("--model_dir", type=str, default="../pth/")
+    parser.add_argument("--agent_id", type=int, default=0)
+    args = parser.parse_args()
+
+    mp.set_start_method("spawn")
+
+    PW = SharelWorker(Agent, args, MTSPEnv)
+    PW.run()
