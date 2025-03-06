0569923df2db7e1dafac100ea040b6d98271877c
jarodf <jarodf>
2025-03-06 15:27:04 +0800

    recover to base rewards

--- envs/MTSP/MTSP3.py
+++ envs/MTSP/MTSP3.py
@@ -223,12 +223,12 @@ class MTSPEnv:
         if np.all(self.traj_stages >= 2):
             max_cost = np.max(self.costs)
             rewards += - max_cost               * 1.0
-            rewards += (max_cost - self.costs)  * 0.05
-            rewards += - self.costs             * 0.05
+            # rewards += (max_cost - self.costs)  * 0.05
+            # rewards += - self.costs             * 0.05
             rewards /= max_cost
-        else:
-            remain_city_num = np.count_nonzero(self.mask)
-            rewards += np.where( np.logical_and(self.traj_stages>=2 , actions == 1), -remain_city_num / self.cities, 0)
+        # else:
+        #     remain_city_num = np.count_nonzero(self.mask)
+        #     rewards += np.where( np.logical_and(self.traj_stages>=2 , actions == 1), -remain_city_num / self.cities, 0)
         # else:
         #     max_cost = np.max(self.costs)
         #     last_max_cost = np.max(self.last_costs)
