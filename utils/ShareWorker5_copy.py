49a5ac444ebdc349970cba22ff16903dbb84ac42
jarodf <jarodf>
2025-03-13 16:31:33 +0800

    参数调整

--- utils/ShareWorker5_copy.py
+++ utils/ShareWorker5_copy.py
@@ -31,18 +31,18 @@ if __name__ == "__main__":
     parser.add_argument("--embed_dim", type=int, default=128)
     parser.add_argument("--num_heads", type=int, default=4)
     parser.add_argument("--num_layers", type=int, default=2)
-    parser.add_argument("--lr", type=float, default=1e-4)
-    parser.add_argument("--grad_max_norm", type=float, default=1.0)
+    parser.add_argument("--lr", type=float, default=2e-4)
+    parser.add_argument("--grad_max_norm", type=float, default=10)
     parser.add_argument("--cuda_id", type=int, default=0)
     parser.add_argument("--use_gpu", type=bool, default=True)
     parser.add_argument("--max_ent", type=bool, default=True)
     parser.add_argument("--entropy_coef", type=float, default=2e-2)
-    parser.add_argument("--accumulation_steps", type=int, default=4)
-    parser.add_argument("--batch_size", type=int, default=4)
+    parser.add_argument("--accumulation_steps", type=int, default=8)
+    parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--city_nums", type=int, default=50)
     parser.add_argument("--model_dir", type=str, default="../pth/")
-    parser.add_argument("--agent_id", type=int, default=0)
-    parser.add_argument("--env_masks_mode", type=int, default=0, help="0 for only the min cost  not allow back depot; 1 for only the max cost allow back depot")
+    parser.add_argument("--agent_id", type=int, default=10000)
+    parser.add_argument("--env_masks_mode", type=int, default=1, help="0 for only the min cost  not allow back depot; 1 for only the max cost allow back depot")
     parser.add_argument("--eval_interval", type=int, default=100, help="eval  interval")
     parser.add_argument("--use_conflict_model", type=bool, default=True, help="0:not use;1:use")
     parser.add_argument("--only_one_instance", type=bool, default=True, help="0:not use;1:use")
@@ -124,5 +124,5 @@ if __name__ == "__main__":
                   f"or_costs:{ortools_cost:.5f},"
                   f"gap:{ gap:.5f}%")
 
-        if (i+1)%args.save_model_interval == 0:
+        if (i+1)%(args.save_model_interval * args.accumulation_steps) == 0:
             agent.save_model(args.agent_id + i+1)
