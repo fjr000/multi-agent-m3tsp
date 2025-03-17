5827a2a88168c6d2a0758174e3c10b3ba235685d
jarodf <jarodf>
2025-03-17 17:08:43 +0800

    cur_pos - depot as embed

--- model/RefModel/AgentAttentionEncoder.py
+++ model/RefModel/AgentAttentionEncoder.py
@@ -10,7 +10,7 @@ class AgentEmbedding(nn.Module):
         self.input_dim = input_dim
         self.hidden_dim = hidden_dim
 
-        self.depot_pos_embed = nn.Linear(self.embed_dim * 2, self.embed_dim)
+        self.depot_pos_embed = nn.Linear(self.embed_dim , self.embed_dim)
         self.distance_cost_embed = nn.Linear(2, self.embed_dim)
         self.next_cost_embed = nn.Linear(3, self.embed_dim)
         self.problem_scale_embed = nn.Linear(1, self.embed_dim)
@@ -28,15 +28,16 @@ class AgentEmbedding(nn.Module):
         """
 
         # cities_expand = cities_embed.expand(agent_state.size(0), -1, -1)
-        depot_pos = cities_embed[torch.arange(agent_state.size(0))[:, None, None], agent_state[:,:,:2].long(),:].reshape(agent_state.size(0),agent_state.size(1), 2*self.embed_dim)
-        depot_pos_embed = self.depot_pos_embed(depot_pos)
+        depot_pos = cities_embed[torch.arange(agent_state.size(0))[:, None, None], agent_state[:,:,:2].long(),:]
+        depot_pos_embed = self.depot_pos_embed(depot_pos[:,:,0,:] - depot_pos[:,:,1,:])
         distance_cost_embed = self.distance_cost_embed(agent_state[:,:,2:4])
         next_cost_embed = self.next_cost_embed(agent_state[:,:,4:7])
         problem_scale_embed = self.problem_scale_embed(agent_state[:,:,7:8])
         global_graph_embed = self.graph_embed(graph_embed).expand_as(depot_pos_embed)
 
         position_embed = self.position_embed(agent_state.size(1))[None, :].expand_as(depot_pos_embed)
-        context = torch.cat([global_graph_embed, depot_pos_embed + distance_cost_embed + next_cost_embed + problem_scale_embed + position_embed ], dim=-1)
+        context =global_graph_embed + depot_pos_embed + distance_cost_embed + next_cost_embed + problem_scale_embed + position_embed
+        # context = torch.cat([global_graph_embed, depot_pos_embed + distance_cost_embed + next_cost_embed + problem_scale_embed + position_embed ], dim=-1)
         agent_embed = self.agent_embed(context)
         return agent_embed
 
